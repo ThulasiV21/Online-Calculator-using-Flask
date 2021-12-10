34209be4e9ecc16ec9756525ba56f6af9903e0ea
thulasigovardhan <thulasigovardhan@gmail.com>
2021-12-10 16:02:25 -0500

    Task: Added class to check for numeric inputs and added pylint and glossary html pages to jinja templates

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 """A simple flask web app"""
 from flask import Flask
-from app.controllers.index_controller import IndexController
-from app.controllers.calculator_controller import CalculatorController
+from controllers.index_controller import IndexController
+from controllers.calculator_controller import CalculatorController
+from controllers.pylint_controller import PylintController
+from controllers.glossary_controller import GlossaryController
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -17,3 +19,11 @@ def calculator_get():
 @app.route("/calculator", methods=['POST'])
 def calculator_post():
     return CalculatorController.post()
+
+@app.route("/pylint", methods=['GET'])
+def pylint_get():
+    return PylintController.get()
+
+@app.route("/glossary", methods=['GET'])
+def glossary_get():
+    return GlossaryController.get()
--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -1,20 +1,22 @@
 from app.controllers.controller import ControllerBase
 from calc.calculator import Calculator
+from calc.utils.input_validator import InputValidator
 from flask import render_template, request, flash, redirect, url_for
 
 
 class CalculatorController(ControllerBase):
     @staticmethod
     def post():
+
+
         if request.form['value1'] == '' or request.form['value2'] == '':
+            error = 'The values cannot be empty'
+            return render_template('calculator.html', error=error)
+        elif not InputValidator(request.form['value1'], request.form['value2']).validate():
             error = 'Please enter numeric values'
             return render_template('calculator.html', error=error)
-        # elif request.form['value1'] = /[^a-zA-Z0-9 ]/g or request.form['value2'] == [/a-zA-Z/]:
-        #     error = 'Please enter numeric values'
-        #     return render_template('calculator.html', error=error)
         else:
             flash('Calculation was successful')
-
             # get the values out of the form
             value1 = request.form['value1']
             value2 = request.form['value2']
