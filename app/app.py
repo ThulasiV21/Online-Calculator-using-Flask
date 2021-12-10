e8b47f47fbcba5b09c9f527825cfc5e4778c1b31
thulasigovardhan <thulasigovardhan@gmail.com>
2021-12-10 16:19:50 -0500

    Task: Added class to check for numeric inputs and added pylint and glossary html pages to jinja templates

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 """A simple flask web app"""
 from flask import Flask
-from controllers.index_controller import IndexController
-from controllers.calculator_controller import CalculatorController
-from controllers.pylint_controller import PylintController
-from controllers.glossary_controller import GlossaryController
+from app.controllers.index_controller import IndexController
+from app.controllers.calculator_controller import CalculatorController
+from app.controllers.pylint_controller import PylintController
+from app.controllers.glossary_controller import GlossaryController
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
