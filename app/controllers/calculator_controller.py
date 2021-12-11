fcc32fda22a9706ac1981dd5f7719d4e03ce20b0
thulasigovardhan <thulasigovardhan@gmail.com>
2021-12-11 16:07:30 -0500

    Task: Refactored webpages

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -7,7 +7,7 @@ from flask import render_template, request, flash, redirect, url_for
 class CalculatorController(ControllerBase):
     @staticmethod
     def post():
-
+        flash('Please provide values by separating them with ";"')
 
         if request.form['value1'] == '' or request.form['value2'] == '':
             error = 'The values cannot be empty'
@@ -16,10 +16,12 @@ class CalculatorController(ControllerBase):
             error = 'Please enter numeric values'
             return render_template('calculator.html', error=error)
         else:
+
             flash('Calculation was successful')
             # get the values out of the form
-            value1 = request.form['value1']
-            value2 = request.form['value2']
+            values = request.form['value'].split(";")
+            # value1 = request.form['value1']
+            # value2 = request.form['value2']
             operation = request.form['operation']
             # make the tuple
             my_tuple = (value1, value2)
